748d266672ae9e1daeef380fa8ac2ce80b57be11
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-01-13 08:25:59 -0600

    augment project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject dupes.clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+  :description "A tool for finding duplicate groupings of words in a large body of text"
+  :license {:distribution :repo
+            :comments "same as Clojure"
+            :name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/NCCTS/dupes.clj"
 
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
