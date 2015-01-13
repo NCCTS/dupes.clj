6b88b3b1d6684398871610d5777d222c99f3b649
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-01-13 08:04:46 -0600

    project.clj (needs work, mostly stock)

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject dupes.clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+
+  :main dupes
+
+  :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]])
