ff167d930a897fac43654914259a5f003546bacf
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-01-18 01:43:04 -0600

    gen tests for max-steps

--- test/kircher/test.clj
+++ test/kircher/test.clj
@@ -24,3 +24,36 @@
     (is (= (k/txt->vec "a b c d")
            ["a" "b" "c" "d"]))))
 
+(deftest max-steps
+  (testing "step <= 0"
+    (let [zero-throws
+          (prop/for-all
+           [coll (gen/vector gen/int)
+            n gen/int
+            ;; neg-int may gen 0 val, while s-neg-int would not
+            step gen/neg-int]
+           ;; wrapping in `boolean' is a hack
+           ;;   http://dev.clojure.org/jira/browse/TCHECK-26
+           (boolean (is (thrown? Error (k/max-steps coll n step))))
+           (boolean (is (thrown? Error (k/max-steps coll n step 'pad)))))]
+      (tc/quick-check 1000 zero-throws)))
+
+  (testing "empty coll"
+    (let [empty-always-zero
+          (prop/for-all
+           [n gen/int
+            ;; s-pos-int never gens 0 val
+            step gen/s-pos-int]
+           (is (= 0 (k/max-steps [] n step)))
+           (is (= 0 (k/max-steps [] n step 'pad))))]
+      (tc/quick-check 1000 empty-always-zero)))
+
+  (testing "n < 0"
+    (let [n-lte-0-always-0-or-1
+          (prop/for-all
+           [coll (gen/such-that not-empty (gen/vector gen/int))
+            n gen/s-neg-int
+            step gen/s-pos-int]
+           (is (= 0 (k/max-steps coll n step)))
+           (is (= 1 (k/max-steps coll n step 'pad))))]
+      (tc/quick-check 1000 n-lte-0-always-0-or-1))))
